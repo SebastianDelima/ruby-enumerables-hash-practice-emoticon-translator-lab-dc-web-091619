deee7812294766a11b37e66fa4f602f8e74dbc31
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-13 23:50:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,24 @@
-# require modules here
+require"yaml"
+require'pry'
 
-def load_library
-  # code goes here
-end
+def load_library(emoticons)
+ hash = YAML.load_file(emoticons)
+ new_hash = {"get_meaning" => {}, "get_emoticon" => {}}
+ hash.each do |emoticon, values|
+   new_hash["get_meaning"][values[1]] = emoticon
+   new_hash["get_emoticon"][values[0]] = values[1]
+   end
+return new_hash
+ end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon )
+hash = load_library(file_path)
+new_hash = hash["get_emoticon"]
+hash.each do |english|
+  binding.pry
+  
 end
 
 def get_english_meaning
-  # code goes here
+  
 end
