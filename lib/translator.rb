114875e5aef02cc1fe9a340825928331e5580786
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-14 00:00:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,12 +16,12 @@ hash = load_library(file_path)
 new_hash = hash["get_emoticon"]
 new_hash.each do |english|
   if english == emoticon do
-    binding.pry
+   
     return english
-    binding.pry
+   
   else
     return "Sorry"
-binding.pry
+
   end
  end
 end
